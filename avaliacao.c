747c2dfe2b64e6fd1336d7c8b3dbfa83fff491fe
GabrielEDROM <gabriel_gx7@yahoo.com.br>
2017-06-16 20:42:35 -0300

    adição das funções do "criacao-da-populacao.c" no arquivo princical "avaliacao.c" para evitar o uso de .headers (por enquanto)

--- avaliacao.c
+++ avaliacao.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #include "criacao-da-populacao.h"
 
@@ -14,9 +15,199 @@
 //operador 2 => Maior ou Igual
 //operador 3 => Menor
 
+typedef struct gene{
+float peso;
+int operador,valor;
+}gene;
+
+typedef struct individuo
+{
+    gene fita[35];
+    float fitness;
+}individuo;
+
+struct individuo populacao[50];
+struct individuo populacaoFilhos[50];
+
 void executaBasePopulacao();
 void executaBaseIndividuo();
 void get_database(int total_data[][ATRIBUTES]);
+
+// |-------------------------------------------------------------
+void criaPopulacao(int tamanho);
+void imprime();
+void crossover(int p1,int p2,int indiceCross);
+void mutacao(int ind);
+void ordena(int popCase);
+void elitismoP();
+
+void ordena(int popCase)
+{
+    int i,j,menor;
+    individuo aux;
+    for(i=0;i<50;i++)
+    {
+        menor=i;
+        for(j=i;j<50;j++)
+        {
+            if(popCase==1)// ordena a populacao
+            {
+            if(populacao[j].fitness<populacao[menor].fitness)
+                {aux=populacao[j];
+                populacao[j]=populacao[menor];
+                populacao[menor]=aux;
+                menor=j;
+                }
+            }
+            else // ordena a populacao gerada por crossover
+            {
+            if(populacaoFilhos[j].fitness<populacaoFilhos[menor].fitness)
+                {aux=populacaoFilhos[j];
+                populacaoFilhos[j]=populacaoFilhos[menor];
+                populacaoFilhos[menor]=aux;
+                menor=j;
+                }
+            }
+        }
+    }
+}
+
+void criaPopulacao(int tamanho)
+{
+    int x=0,i;
+    while(x<tamanho)
+    {
+        for(i=0;i<35;i++)
+        {
+            //peso de ativa�ao com 3 casas decimais entre 0 e 1.
+            populacao[x].fita[i].peso=(rand()%1000)/1000.0;
+
+            if(i!=10)
+                populacao[x].fita[i].operador=rand()%4; //campo operador dos genes.
+            else
+                populacao[x].fita[i].operador=rand()%2; //campo operador do gene historico familiar.
+
+            if(i==10)populacao[x].fita[i].valor=rand()%2; //historico familiar � binario.
+            if(i==33)populacao[x].fita[i].valor=rand()%76+1; //gene idade.
+            if(i==34)
+            {
+                populacao[x].fita[i].peso=1;
+                populacao[x].fita[i].operador=0;
+                populacao[x].fita[i].valor=(rand()%6)+1; //gene que contem o tipo.
+            }
+        }
+        x++;
+    }
+}
+
+void imprime()
+{
+    int x,i, jump=0;
+    for(x=0;x<50;x++)
+    {
+        for(i=0;i<35;i++)
+        {
+            if(populacao[x].fita[i].peso >= WEIGHT_LIMIT) // apenas os pesos menos que 0.3 ativam, se quiser pode ser >0.7 | de qualquer jeito passao 30%
+            {
+              jump = 1;
+              //printf("- %.3f",populacao[x].fita[i].peso); // se quiser ver o peso
+              printf("gen%d ",i); //indica o gene.
+              if(populacao[x].fita[i].operador==0)
+                printf("== %d ",populacao[x].fita[i].valor);
+              if(populacao[x].fita[i].operador==1)
+                printf("!= %d ",populacao[x].fita[i].valor);
+              if(populacao[x].fita[i].operador==2)
+                printf(">= %d ",populacao[x].fita[i].valor);
+              if(populacao[x].fita[i].operador==3)
+                printf("< %d ",populacao[x].fita[i].valor);
+              //printf(" and ");
+            }
+            // if(populacao[x].fita[i].peso  WEIGHT_LIMIT)
+            // {
+            //   printf("\n");
+            //   printf("\n-----------------------------\n");
+            // }
+        }
+      if(jump==1) printf("\n");
+      jump=0;
+    }
+}
+
+void crossover(int p1,int p2,int indiceCross)
+{
+    individuo f1,f2;
+    int a,b,i;
+    a=rand()%34;+1;
+    b=rand()%(34-a);
+    for(i=0;i<34;i++)
+    {
+        if(i<a)
+        {
+            f1.fita[i]=populacao[p1].fita[i];
+            f2.fita[i]=populacao[p2].fita[i];
+        }
+        else{
+        if(i<a+b)
+        {
+            f2.fita[i]=populacao[p1].fita[i];
+            f1.fita[i]=populacao[p2].fita[i];
+        }
+        else
+        {
+            f1.fita[i]=populacao[p1].fita[i];
+            f2.fita[i]=populacao[p2].fita[i];
+        }
+        }
+    }
+    populacaoFilhos[indiceCross]=f1;
+    populacaoFilhos[indiceCross+1]=f2;
+    mutacao(indiceCross);
+    mutacao(indiceCross);
+}
+
+void mutacao(int ind)
+{
+    int a,i;
+
+    for(i=0;i<34;i++)
+    {
+		//mutacao de 30% em cada campo de todos os individuos gerados
+        if(rand()%100<30)
+        {
+            int k=rand()%2;
+            if(k==0)
+            populacaoFilhos[ind].fita[i].peso-=(rand()%10000)/10000.0;
+            else
+                populacaoFilhos[ind].fita[i].peso+=(rand()%10000)/10000.0;
+            if(populacaoFilhos[ind].fita[i].peso<0)populacaoFilhos[ind].fita[i].peso=0;
+            if(populacaoFilhos[ind].fita[i].peso>1)populacaoFilhos[ind].fita[i].peso=1;
+        }
+        if(rand()%100<30)
+			{
+                if(i!=10)populacaoFilhos[ind].fita[i].operador=rand()%4;
+                else populacaoFilhos[ind].fita[i].operador=rand()%2;
+            }
+        if(rand()%100<30)
+        {
+                if(i==34)
+                {
+                 populacaoFilhos[ind].fita[i].valor=rand()%76+1;
+                }
+                else{
+            if(i!=10)populacaoFilhos[i].fita[i].valor=rand()%4;
+                else populacaoFilhos[i].fita[i].valor=rand()%2;
+                }
+        }
+    }
+}
+
+ void elitismoP() // elitismo com apenas o melhor
+ {
+     int i;
+     for(i=1;i<50;i++)
+        populacao[i]=populacaoFilhos[i];
+ }
+//|-------------------------------------------------------------------------------
 
 void executaBasePopulacao(int total_data[][ATRIBUTES])
 {
@@ -70,7 +261,7 @@ void executaBasePopulacao(int total_data[][ATRIBUTES])
     }
     Se = Tp/(Tp+Fn);
     Sp = Tn/(Tn+Fp);
-    populacao.fitness = Se*Sp;
+    populacao[i].fitness = Se*Sp;
     Tp = 0; //para começar a análise da próxima regra (indivíduo)
     Fp = 0; //para começar a análise da próxima regra (indivíduo)
     Tn = 0; //para começar a análise da próxima regra (indivíduo)
@@ -113,6 +304,9 @@ void get_database(int total_data[][ATRIBUTES]) //coleta dos dados do arquivo
 
 int main()
 {
+  clock_t begin = clock();
+  clock_t end;
+  double time_spent;
   int total_data[INSTANCES][ATRIBUTES];
 
   int geracao,execucao=0;
@@ -140,7 +334,8 @@ int main()
 
   while(execucao<10)
   {
-      if(gotTheFile) srand(execucao);
+    printf("\nEXEC%d: \n", execucao);
+      if(!gotTheFile) srand(execucao);
       geracao=0;
       criaPopulacao(50);
       executaBasePopulacao(total_data); // vai executar a base e calcular o fitness
@@ -152,7 +347,7 @@ int main()
       {
         /*
         int pai1=torneio estocastico();
-        int pai2=tornei estocastico();
+        int pai2=torneio estocastico();
         crossover(p1,p2,indiceCross);
         */
       }
@@ -165,6 +360,8 @@ int main()
     }
     execucao++;
   }
-
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("\n\nTime: %lf\n",time_spent);
   return 0;
 }
